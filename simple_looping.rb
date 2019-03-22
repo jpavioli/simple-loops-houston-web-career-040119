b1b44a02fe6aeefe8a5cc9f6c97bbcaa0c1cc09d
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-22 02:37:00 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,7 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  #for counter (1..number_of_times) do
+  for counter in 1..number_of_times do
     puts phrase
-  #end
+  end
 end
